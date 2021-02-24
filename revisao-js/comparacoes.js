6c1fa94b5280b0d518bbc93627b4bcea12ac99c0
Gabriel-Batarra <gabriel.batarra123@gmail.com>
2021-02-24 15:34:54 +0000

    (24/02) Início do algoritmo de busca sequencial

---
+++ revisao-js/comparacoes.js
@@ -0,0 +1,7 @@
+let a = 10, b = 15, c = 10 // Números
+console.log(a > b)
+console.log(a < b)
+console.log(a == c)
+console.log(a != c)
+
+console.log('-------------------------------------')
---
+++ revisao-js/tabela1.js
@@ -0,0 +1,5 @@
+let tabela = {}
+tabela.idades = '15, 15, 15, 30, 20, 20, 20, 30, 30, 20'
+tabela.Nfunc = '2, 3, 1, 5, 9'
+console.log(tabela)
+console.table(tabela)
